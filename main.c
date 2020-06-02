b83d50404988a9fadf5ce3d0ed6e415a87ab8f87
Ksenia Rakcheeva <kseniarakcheeva@yandex.ru>
2020-06-03 00:07:25 +0300

    a+b

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
+
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    printf("Hello World");
-
-    return 0;
+    int a,b,c;
+    
+    printf("введите первое число ");
+    scanf("%d",&a);
+    printf("введите второе число ");
+    scanf("%d",&b);
+    c=a+b;
+    printf("Сумма числе равна %d", c);
+   return 0;
 }
